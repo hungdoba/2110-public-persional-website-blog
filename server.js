10830f66a3624ff645c607ef70f7d29fc330daf7
Jeiv <hungdoba.hdb@gmail.com>
2021-10-27 16:01:22 +0900

    asdfa

--- server.js
+++ server.js
@@ -43,5 +43,7 @@ if (process.env.NODE_ENV === "production") {
 }
 // End Deploy
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});

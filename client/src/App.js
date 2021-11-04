3c75d26a9454ced42f2bd6fc326f888c7c3cf19d
Jeiv <hungdoba.hdb@gmail.com>
2021-11-05 08:52:52 +0900

    work

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,15 @@ import Blog from "./pages/Blog/Blog";
 import Post from "./pages/Post/Post";
 import Search from "./pages/Search";
 import Article from "./pages/Article";
+import { Redirect } from "react-router-dom";
 
 function App() {
     return (
         <HashRouter>
             <Switch>
-                <Route path="/" component={Blog} exact />
+                <Route path="/" component={Blog} exact>
+                    <Redirect from="/#/welcome.html" to="/welcome.html" />
+                </Route>
                 <Route path="/post/:postId" component={Post} exact />
                 <Route path="/article/:articleId" component={Article} exact />
                 <Route path="/search" component={Search} exact />
